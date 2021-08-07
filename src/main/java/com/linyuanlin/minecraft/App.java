47f65f8f81d3cc467a30b69f605880e6585302e6
Yuanlin <im.yuanlinLin@gmail.com>
2021-08-07 18:43:01 +0800

    🎉  [init] Created an empty minecraft plugin

---
+++ src/main/java/com/linyuanlin/minecraft/App.java
@@ -0,0 +1,16 @@
+package com.linyuanlin.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
