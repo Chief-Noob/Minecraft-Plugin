018395e653a0a40e34b3c6e3a0f75be3c7f9ed2c
Yuanlin <im.yuanlinLin@gmail.com>
2021-08-09 01:34:15 +0800

    :bug: fix merge error

--- src/main/java/com/linyuanlin/minecraft/App.java
+++ src/main/java/com/linyuanlin/minecraft/App.java
@@ -1,38 +1,29 @@
 package com.linyuanlin.minecraft;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
 import com.linyuanlin.minecraft.models.PlayerData;
+import com.linyuanlin.minecraft.models.Team;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
-import com.linyuanlin.minecraft.models.Team;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 public class App extends JavaPlugin implements Listener {
@@ -91,7 +82,9 @@ public class App extends JavaPlugin implements Listener {
         Bukkit.getScheduler().callSyncMethod(this, () -> this.setholo(e.getPlayer(), e.getMessage(), 1)).get();
     }
 
-    /** 對話時產生 hologram 在玩家頭上 */
+    /**
+     * 對話時產生 hologram 在玩家頭上
+     */
     public boolean setholo(Player Player, String msg, int second) {
 
         final Hologram hologram = HologramsAPI.createHologram(this, Player.getLocation().add(0.0, 2.0, 0.0));
@@ -135,9 +128,57 @@ public class App extends JavaPlugin implements Listener {
 
     @EventHandler
     public boolean onCommand(CommandSender sender, Command cmd, String cmdlable, String[] args) {
-        Player p = (Player) sender;
+        Player p1 = (Player) sender;
         if (cmdlable.equals("team")) {
-            p.sendMessage("組隊功能正在開發中");
+
+            switch (args[0]) {
+                case "invite": {
+                    Player p = Bukkit.getPlayer(args[1]);
+                    TextComponent a = new TextComponent("[確認組隊邀請]");
+                    a.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("點擊接受 " + sender.getName() + " 的組隊邀請 ")));
+                    a.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/team join " + args[1]));
+                    p.spigot().sendMessage(a);
+                    break;
+                }
+                case "join": {
+                    Player p = Bukkit.getPlayer(args[1]);
+                    TextComponent msg = new TextComponent("");
+                    if (allPlayers.get(p1.getUniqueId()).team.isEmpty()) {
+                        List<PlayerData> playerArray = new ArrayList<>();
+                        playerArray.add(allPlayers.get(p1.getUniqueId()));
+                        playerArray.add(allPlayers.get(p.getUniqueId()));
+                        Team team = new Team(playerArray);
+                        allPlayers.get(p1.getUniqueId()).team = team;
+                        allPlayers.get(p.getUniqueId()).team = team;
+                        team.leader = allPlayers.get(p1.getUniqueId());
+                        msg = new TextComponent(p.getName() + "已加入" + "(" + allPlayers.get(p1.getUniqueId()).team.size() + "/4");
+                    } else if (!allPlayers.get(p1.getUniqueId()).team.isFull()) {
+                        allPlayers.get(p1.getUniqueId()).team.playerList.add(allPlayers.get(Bukkit.getPlayer(args[0]).getUniqueId()));
+                        allPlayers.get(Bukkit.getPlayer(p.getName()).getUniqueId()).team = allPlayers.get(p1.getUniqueId()).team;
+                        msg = new TextComponent(p.getName() + "已加入" + "(" + allPlayers.get(p1.getUniqueId()).team.size() + "/4");
+                    } else {
+                        msg = new TextComponent("隊伍已滿");
+                    }
+                    p.spigot().sendMessage(msg);
+                    p1.spigot().sendMessage(msg);
+                    break;
+                }
+                case "list": {
+                    StringBuilder teamMemberNameString = new StringBuilder();
+                    for (int i = 0; i < allPlayers.get(p1.getUniqueId()).team.size(); i++) {
+                        teamMemberNameString.append(allPlayers.get(p1.getUniqueId()).team.playerList.get(i).player.getName()).append(" ");
+                    }
+                    TextComponent msg = new TextComponent("隊伍成員：" + teamMemberNameString);
+                    for (int i = 0; i < allPlayers.get(p1.getUniqueId()).team.size(); i++) {
+                        allPlayers.get(p1.getUniqueId()).team.playerList.get(i).player.spigot().sendMessage(msg);
+                    }
+
+                }
+                break;
+                default:
+                    break;
+            }
+
             return true;
         }
         return false;
