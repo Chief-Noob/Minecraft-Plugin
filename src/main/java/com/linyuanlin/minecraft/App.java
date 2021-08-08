a7e0e85cf42d6686df6444cd0a3a989755c9b3eb
sleepymalc <bensoniscool123@gmail.com>
2021-08-09 03:57:44 +0800

    📍[main/App] Team leaving system fined.

--- src/main/java/com/linyuanlin/minecraft/App.java
+++ src/main/java/com/linyuanlin/minecraft/App.java
@@ -215,10 +215,14 @@ public class App extends JavaPlugin implements Listener {
                         p1.spigot().sendMessage(new TextComponent("你沒有隊伍"));
                         return false;
                     }
-
-                    if (team.get().size() > 1) {
-                        team.get().playerList.remove((Object) allPlayers.get(p1.getUniqueId()));
-                        //Optional<Team> newTeam = Optional.of(new Team(team.get().playerList)); 
+                    
+                    team.get().playerList.remove((Object) allPlayers.get(p1.getUniqueId()));
+                    if (team.get().size() == 1){//2 members-Team, this team should be deleted.
+                        for (PlayerData pd : allPlayers.get(p1.getUniqueId()).team.get().playerList) {
+                            pd.player.spigot().sendMessage(new TextComponent(p1.getName() + " 離開了隊伍, 隊伍人數不足，自動解散"));
+                            pd.team = Optional.empty();
+                        } 
+                    }else if (team.get().size() > 1) {//>2 members-Team, this Team should be remained.  
                         if (team.get().leader == allPlayers.get(p1.getUniqueId())) {
                             for (PlayerData pd : allPlayers.get(p1.getUniqueId()).team.get().playerList) {
                                 if (team.get().leader != p1) {
@@ -228,13 +232,13 @@ public class App extends JavaPlugin implements Listener {
                             }
                             for (PlayerData pd : allPlayers.get(p1.getUniqueId()).team.get().playerList) {
                                 pd.player.spigot().sendMessage(new TextComponent(
-                                        "隊長 " + p1.getName() + " 離開了隊伍, 新隊長為" + team.get().leader.player.getName()));
-                                //pd.team = newTeam;
+                                        "隊長 " + p1.getName() + " 離開了隊伍, 新隊長為" + team.get().leader.player.getName()+ "("
+                                        + allPlayers.get(p1.getUniqueId()).team.get().size() + "/4)"));
                             }
                         } else {
                             for (PlayerData pd : allPlayers.get(p1.getUniqueId()).team.get().playerList) {
-                                pd.player.spigot().sendMessage(new TextComponent("隊員 " + p1.getName() + " 離開了隊伍"));
-                                //pd.team = newTeam;
+                                pd.player.spigot().sendMessage(new TextComponent("隊員 " + p1.getName() + " 離開了隊伍"+ "("
+                                + allPlayers.get(p1.getUniqueId()).team.get().size() + "/4)"));
                             }
                         }
                     }
