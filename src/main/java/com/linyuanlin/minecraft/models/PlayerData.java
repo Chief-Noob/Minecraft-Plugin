8b0b79179bcb1a5ddc043c1ae4bf3b80a12f2b46
SwarzChen <aaaa102234@gmail.com>
2021-08-10 02:22:24 +0800

    :bug: fix connection entity

--- src/main/java/com/linyuanlin/minecraft/models/PlayerData.java
+++ src/main/java/com/linyuanlin/minecraft/models/PlayerData.java
@@ -3,56 +3,58 @@ package com.linyuanlin.minecraft.models;
 import com.linyuanlin.minecraft.App;
 import com.linyuanlin.minecraft.mongodb.MongodbClient;
 import com.mongodb.BasicDBObject;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Date;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
 public class PlayerData {
-    public Player player;
-    public int balance = 0;
-    public BasicDBObject mongoObject;
-    public Optional<Team> team = Optional.empty();
-    private App app;
-    public HashMap<Player, Date> invitedTime = new HashMap<>();
+  public Player player;
+  public int balance = 0;
+  public BasicDBObject mongoObject;
+  public Optional<Team> team = Optional.empty();
+  private App app;
+  public HashMap<Player, Date> invitedTime = new HashMap<>();
 
-    public PlayerData(App app, UUID uuid) throws Exception {
-        this.app = app;
+  public PlayerData(App app, UUID uuid) throws Exception {
+    this.app = app;
 
-        this.player = Bukkit.getServer().getPlayer(uuid);
+    this.player = Bukkit.getServer().getPlayer(uuid);
 
-        if (this.player == null)
-            throw new Exception("PLAYER_NOT_ONLINE");
+    this.mongoObject = new BasicDBObject();
 
-        /* Import data from database */
+    if (this.player == null) throw new Exception("PLAYER_NOT_ONLINE");
 
-        player.sendMessage(ChatColor.GRAY + "你的資料已從資料庫同步完成");
-    }
+    /* Import data from database */
 
-    public void wrapMongoObject() {
-        this.mongoObject.append("balance", this.balance);
-    }
+    player.sendMessage(ChatColor.GRAY + "你的資料已從資料庫同步完成");
+  }
 
-    public void saveData() {
-        /* Save data into database */
-        MongodbClient client = new MongodbClient(this.app, "PlayerData");
-        this.wrapMongoObject();
-        client.insert(this.mongoObject);
+  public void wrapMongoObject() {
+    this.mongoObject.append("balance", this.balance);
+  }
 
-        player.sendMessage(ChatColor.GRAY + "你的資料已自動保存至資料庫");
-    }
+  public void saveData() {
+    /* Save data into database */
+    MongodbClient client = new MongodbClient(this.app, "PlayerData");
+    this.wrapMongoObject();
+    client.insert(this.mongoObject);
 
-    public boolean inviteIsCooling(Player p) {
-        Date date = new Date(System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(1));
-        return date.after(invitedTime.get(p));
-    }
+    player.sendMessage(ChatColor.GRAY + "你的資料已自動保存至資料庫");
+  }
 
-    public boolean isInvitedBy(PlayerData p) {
-        return p.invitedTime.get(this.player) != null;
-    }
+  public boolean inviteIsCooling(Player p) {
+    Date date = new Date(
+      System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(1)
+    );
+    return date.after(invitedTime.get(p));
+  }
+
+  public boolean isInvitedBy(PlayerData p) {
+    return p.invitedTime.get(this.player) != null;
+  }
 }
