02170d48084e880b6ec24fb2aff498f3cfc5833c
sleepymalc <bensoniscool123@gmail.com>
2021-08-16 03:43:37 +0800

    🐛[main/LocationManager] Debug

--- src/main/java/com/linyuanlin/minecraft/Manager/LocationManager.java
+++ src/main/java/com/linyuanlin/minecraft/Manager/LocationManager.java
@@ -4,16 +4,26 @@ import com.linyuanlin.minecraft.App;
 import java.util.*;
 import org.bson.Document;
 import org.bukkit.*;
+import com.mongodb.client.model.Filters;
 
 public class LocationManager {
 	private App app;
-	private HashMap<String, Location> tagLocationMap = new HashMap<>();
+	private HashMap<String, Location> tagLocationMap;
 
 	public LocationManager(App app) {
 		this.app = app;
 	}
 
 	public void loadLocations() {
+		Document newDocument = new Document();
+		newDocument.append("tag", "lobby_spawn");
+		newDocument.append("x", 0.5);
+		newDocument.append("y", 67);
+		newDocument.append("z", 0.5);
+
+		app.dbClient.insert("Location", newDocument);
+
+		this.tagLocationMap = new HashMap<>();
 		Document doc = app.dbClient.findOne("Location", "tag", "lobby_spawn");
 		tagLocationMap.put("lobby_spawn",
 				new Location(this.app.worldManager.getWorldData(WorldManager.world_lobby).world,
