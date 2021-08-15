3fe45d76378eb7bde58e0edabae27c8cd65cb9d7
sleepymalc <bensoniscool123@gmail.com>
2021-08-15 22:41:59 +0800

    🔨[main/CustomItem] Move guildInvitationPaper to CustomItem

--- src/main/java/com/linyuanlin/minecraft/Manager/GuildManager.java
+++ src/main/java/com/linyuanlin/minecraft/Manager/GuildManager.java
@@ -2,6 +2,7 @@ package com.linyuanlin.minecraft.manager;
 
 import com.linyuanlin.minecraft.App;
 import com.linyuanlin.minecraft.models.PlayerData;
+import com.linyuanlin.minecraft.item.CustomItem;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,8 +11,8 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.*;
 
+import org.bukkit.*;
 import java.util.*;
 import java.io.*;
 
@@ -48,14 +49,7 @@ public class GuildManager implements CommandExecutor {
 	}
 
 	private boolean getInvitationPaper(PlayerData senderPlayer) {
-		ItemStack stack = new ItemStack(Material.PAPER, 1);
-		ItemMeta im = stack.getItemMeta();
-		im.setDisplayName(ChatColor.DARK_PURPLE + "公會創立卷");
-		im.setLore(Arrays.asList("", ChatColor.WHITE + "持本卷點擊右鍵即可創立公會，", ChatColor.WHITE + "使用本卷的玩家將成為公會會長",
-				ChatColor.WHITE + "在一天內需要有至少三人找到相應NPC附議"));
-		stack.setItemMeta(im);
-
-		senderPlayer.player.getInventory().addItem(new ItemStack(stack));
+		senderPlayer.player.getInventory().addItem(new ItemStack(CustomItem.guildInvitationPaper));
 
 		return true;
 	}
@@ -65,7 +59,7 @@ public class GuildManager implements CommandExecutor {
 		senderPlayer.player.sendMessage(ChatColor.AQUA + "/guild 的使用方式：");
 
 		senderPlayer.player.sendMessage("/guild getInvitationPaper" + ChatColor.GRAY + " - 獲得公會創立卷");
-		
+
 		senderPlayer.player.sendMessage(ChatColor.GRAY + "/guild help - 取得幫助");
 		return false;
 	}
